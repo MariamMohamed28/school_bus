d4e4bb78f9ab3a7ce27b637e132b6fed032da561
MariamMohamed28 <90985629+MariamMohamed28@users.noreply.github.com>
2022-03-27 10:24:53 +0200

    Add files via upload

---
+++ school_bus-master/lib/model/school_user.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+
+class SchoolUserModel {
+  bool isUpdated;
+
+  String name;
+  String email;
+  String phone;
+  String uid;
+  String profileImage;
+  String childName;
+  String chlildAddress;
+  String schoolName;
+  String schoollocation;
+
+  SchoolUserModel({
+    this.isUpdated,
+    this.email,
+    this.name,
+    this.phone,
+    this.uid,
+    this.profileImage,
+    this.childName,
+    this.chlildAddress,
+    this.schoolName,
+    this.schoollocation,
+  });
+  SchoolUserModel.fromjson(Map<String, dynamic> json) {
+    email = json['email'];
+    if (!isUpdated) {
+      name = json['name'];
+    }
+    phone = json['phone'];
+    uid = json['uid'];
+
+    profileImage = json['profileImage'];
+    childName = json['childName'];
+    chlildAddress = json['chlildAddress'];
+    schoolName = json['schoolName'];
+    schoollocation = json['schoollocation'];
+  }
+  Map<String, dynamic> toMap() {
+    return {
+      "name": name,
+      "email": email,
+      "phone": phone,
+      "uid": uid,
+      "profileImage": profileImage,
+      "childName": childName,
+      "chlildAddress": chlildAddress,
+      "schoolName": schoolName,
+      "schoollocation": schoollocation,
+    };
+  }
+}
+
+class SchoolProfileModel {
+  String phone;
+  String uid;
+  String image;
+  String childName;
+  String chlildAddress;
+  String schoolName;
+  String schoollocation;
+
+  SchoolProfileModel({
+    this.phone,
+    this.image,
+    this.childName,
+    this.chlildAddress,
+    this.schoolName,
+    this.schoollocation,
+    this.uid,
+  });
+  SchoolProfileModel.fromjson(Map<String, dynamic> json) {
+    phone = json['phone'];
+    uid = json['uid'];
+
+    image = json['image'];
+    childName = json['childName'];
+    chlildAddress = json['chlildAddress'];
+    schoolName = json['schoolName'];
+    schoollocation = json['schoollocation'];
+  }
+  Map<String, dynamic> toMap() {
+    return {
+      "phone": phone,
+      "image": image,
+      "childName": childName,
+      "chlildAddress": chlildAddress,
+      "schoolName": schoolName,
+      "schoollocation": schoollocation,
+      "uid": uid,
+    };
+  }
+}
